70dbe017f01e95510ebf59e7692fc9304c37b4f1
mouadbz <57324354+mouadbz@users.noreply.github.com>
2024-01-09 13:35:14 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -30,9 +30,10 @@ const LinkComponent = () => {
   };
 
   const handleExtraButtonClick = () => {
-    navigator.clipboard.writeText("I found a solution by using agency accounts. This problem mostly happens due to credit card issues, sometimes involving name or location mismatches. After trying many accounts and sending numerous appeals without success, I turned to agency accounts. Here is an article on Reddit that explains how they work and how to obtain them.
-https://www.reddit.com/r/agencyaccs/comments/19183o5/google_agency_accounts/");
+    navigator.clipboard.writeText('I found a solution by using agency accounts. This problem mostly happens due to credit card issues, sometimes involving name or location mismatches. After trying many accounts and sending numerous appeals without success, I turned to agency accounts. Here is an article on Reddit that explains how they work and how to obtain them.\nhttps://www.reddit.com/r/agencyaccs/comments/19183o5/google_agency_accounts/');
     setExtraButton("Copied!");
+  
+  
 
     // Set a timeout to change the button back to its initial state after 1000 milliseconds (1 second)
     setTimeout(() => {
